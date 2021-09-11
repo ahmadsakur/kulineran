9f062b6ca68432ff1c9ab25b20c6dfa5708d201c
ahmadsakur <sakuretsuu@gmail.com>
2021-09-11 16:20:06 +0700

    Create Dashboard UI

---
+++ src/components/Hero.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row mt-4">
+    <div class="col">
+      <div class="d-flex h-100">
+        <div class="justify-content-center align-self-center">
+          <h1>
+            <strong>Delicious Food</strong> <br />
+            at your fingertips
+          </h1>
+          <p>Fresh food available arround, get yours now!</p>
+          <button class="btn btn-success">
+            Order Now <b-icon-arrow-right></b-icon-arrow-right>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <img src="../assets/img/ilustrasi.png" width="100%" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style></style>
